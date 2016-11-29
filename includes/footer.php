ce1aaa6a3d9fe6fefcd6dc4e579c7eae992bd957
Marcos Dreveck <marcoshsdreveck@gmail.com>
2016-11-29 16:58:21 -0200

    Home mais scrol menu

--- includes/footer.php
+++ includes/footer.php
@@ -8,7 +8,29 @@
       <li><a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
     </ul>
   </div>
-
-  </p>
 </footer>
+
+<script>
+/*Alterar menu ao rolar tela*/
+$(document).ready(function($){
+    var nav1 = $('.menu');
+    var nav2 = $('.menuF');
+    var nav3 = $('.redesSociais');
+    var nav4 = $('.search');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 10) {
+            nav4.addClass("searchScroll");
+            nav1.addClass("semMenu");
+            nav2.addClass("menuFixo")
+            nav3.addClass("semMenu");
+        } else {
+            nav4.removeClass("searchScroll");
+            nav1.removeClass("semMenu");
+            nav2.removeClass("menuFixo")
+            nav3.removeClass("semMenu");
+        }
+    });
+});
+
+</script>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -7,24 +7,26 @@
     <link href="lib/font-awesome/css/font-awesome.min.css" type="text/css" rel="stylesheet" />
   </head>
 <header>
-  <div class="logotipo"> </div>
-  <nav class="menu">
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Categorias</a></li>
-      <li><a href="#">Envie uma dica</a></li>
-      <li><a href="#">Contato</a></li>
-    </ul>
-  </nav>
-  <div class="redesSociais">
-    <ul>
-      <li><a href="#"><i class="fa fa-reddit-alien" aria-hidden="true"></i></a></li>
-      <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-      <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-      <li><a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
-    </ul>
-  </div>
-  <div class="search">
-    <input type="search" placeholder="Search..."/><i class="fa fa-search" aria-hidden="true"></i>
+  <div class="menuF">
+    <div class="logotipo"> </div>
+    <nav class="menu">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Categorias</a></li>
+        <li><a href="#">Envie uma dica</a></li>
+        <li><a href="#">Contato</a></li>
+      </ul>
+    </nav>
+    <div class="redesSociais">
+      <ul>
+        <li><a href="#"><i class="fa fa-reddit-alien" aria-hidden="true"></i></a></li>
+        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+        <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+        <li><a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+      </ul>
+    </div>
+    <div class="search /*searchScroll*/">
+      <input type="search" placeholder="Search..."/><i class="fa fa-search" aria-hidden="true"></i>
+    </div>
   </div>
 </header>
